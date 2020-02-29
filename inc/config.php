1653f6a740e48cee2ec0eb327218721d071857bd
Grisha Belinsky <grishabelinsky@hotmail.com>
2020-03-01 00:51:19 +0800

    modified config

--- inc/config.php
+++ inc/config.php
@@ -3,13 +3,13 @@
 / Set API credentials
 */
 $appConf = [
-    'api_key' => 'bb3cc841f72dbd30003bd62f5cbccc98',  // API key of shopify app
-    'shared_secret' => '036a40405a4cb865376af23dfcf168c2', // Secret key of shopify app
-    'app_url' => 'https://scent-sation.co.uk/include/zeptoapp', // Sourcce code url on Server
+    'api_key' => 'bb3cc841f72dbd30003bd62f5cb******',  // API key of shopify app
+    'shared_secret' => '036a40405a4cb865376af23******', // Secret key of shopify app
+    'app_url' => 'https://example.com/include/zeptoapp', // Sourcce code url on Server
     'app_name' => 'zeptoapp', // Shopify App name
     'shop_name' => 'zeptoappdev', // Shopify store name example zeptoappdev.shopify.com
     'token_file' => 'data/token.txt', // Token file of shopify and server
     'postcode_file' => 'data/postcodes.txt', // Postcodes of shopify store
-    'zepto_app_id' => '11377064023', // ZeptoExpress API id, it should be registered
-    'zepto_token' => '601e08ae2068b9b83750a86725c5fd07' // ZeptoExpress API token
+    'zepto_app_id' => '113770****', // ZeptoExpress API id, it should be registered
+    'zepto_token' => '601e08ae2068b9b83750a8672******' // ZeptoExpress API token
 ];
